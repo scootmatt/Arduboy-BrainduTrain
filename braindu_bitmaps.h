210de149bfb4679bec286f1301a120f8f2668288
scootmatt <scootmatt@hotmail.com>
2016-06-03 08:13:26 +0000

    Add files via upload

---
+++ braindu_bitmaps.h
@@ -0,0 +1,18 @@
+#ifndef BRAINDU_BITMAPS_H
+#define BRAINDU_BITMAPS_H
+
+#include <avr/pgmspace.h>
+
+extern const unsigned char title_brain[];   //32 x 24
+extern const unsigned char LH_arrow[];      //4 x 7
+extern const unsigned char RH_arrow[];      //4 x 7
+extern const unsigned char menu_tile_0[];   //17 x 16
+extern const unsigned char menu_tile_1[];   //17 x 16
+extern const unsigned char menu_tile_2[];   //17 x 16
+extern const unsigned char menu_tile_3[];   //17 x 16
+extern const unsigned char menu_tile_4[];   //17 x 16
+extern const unsigned char menu_tile_5[];   //17 x 16
+extern const unsigned char menu_tile_6[];   //17 x 16
+extern const unsigned char menu_tile_7[];   //17 x 16
+extern const unsigned char menu_tile_8[];   //17 x 16
+#endif
